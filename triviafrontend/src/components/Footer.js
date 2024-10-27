392e1b97b283228a367ceccef7f66835aea8af36
Dylan-Petok <156538364+Dylan-Petok@users.noreply.github.com>
2024-10-26 20:24:19 -0400

    basic front-end navigation and structure set up

---
+++ triviafrontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <p>&copy; 2024 The Build Guild. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ triviafrontend/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to Trivia!</h1>
+            <p>Test your knowledge with our trivia questions!</p>
+        </div>
+    );
+};
+
+export default HomePage;
